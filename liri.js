6fd640d3cde7c1a9108673352d3fe90989d92b3e
Cristopher Ayala Paz <crisayala16@gmail.com>
2017-04-12 18:41:49 -0400

    pretty code

--- liri.js
+++ liri.js
@@ -1,31 +1,37 @@
 var fs = require("fs");
 var Twitter = require("twitter");
 var command = process.argv[2];
-fs.readFile("keys.js", "utf-8", function(error, data){
-	var keys = data.split("=");
-	var twitterKeys = keys[1];
-	splitKeys = twitterKeys.split("'");
-	var consumerKey = splitKeys[1];
-	var consumerSecret = splitKeys[3];
-	var accessTokenKey = splitKeys[5];
-	var accessTokenSecret = splitKeys[7];
-	var client = new Twitter({
-		consumer_key: consumerKey,
-  		consumer_secret: consumerSecret,
-  		access_token_key: accessTokenKey,
-  		access_token_secret: accessTokenSecret
-	});
-	var params = {
-		screen_name: 'CrisAP28',
-		count: "20"
-	};
-	client.get('statuses/user_timeline', params, function(error, tweets, response){
-		if(command === "my-tweets"){
-			for(var i = 0; i < tweets.length; i++){
-				console.log(i + 1 + ": " + tweets[i].text);
-			}
-		}
-	})
+//reading keys.js file in order to use data
+fs.readFile("keys.js", "utf-8", function(error, data) {
+    //spliting the data in order to get the twitterKeys section 
+    var keys = data.split("=");
+    var twitterKeys = keys[1];
+    //spliting the twitterKeys seciton in order to seperate the keys seperatly
+    var splitKeys = twitterKeys.split("'");
+    //asigning the keys into variables
+    var consumerKey = splitKeys[1];
+    var consumerSecret = splitKeys[3];
+    var accessTokenKey = splitKeys[5];
+    var accessTokenSecret = splitKeys[7];
+    //Twitter package stuff
+    var client = new Twitter({
+        consumer_key: consumerKey,
+        consumer_secret: consumerSecret,
+        access_token_key: accessTokenKey,
+        access_token_secret: accessTokenSecret
+    });
+    //Setting the parameters
+    var params = {
+        screen_name: 'CrisAP28',
+        count: "20"
+    };
+    //Actual api request to retrieve the tweets
+    client.get('statuses/user_timeline', params, function(error, tweets, response) {
+        if (command === "my-tweets") {
+            for (var i = 0; i < tweets.length; i++) {
+                console.log(i + 1 + ": " + tweets[i].text);
+            }
+        }
+    })
 
 });
- 
